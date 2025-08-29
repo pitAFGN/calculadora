d588baff8d414e7e0c2a8a22ec3b068f24d59447
Jean Carlo Salazar Gracia <larserxd@gmail.com>
2025-08-29 16:37:52 -0500

    commit2

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function sumar(num1, num2) {
   return num1 + num2;
 }
 
-
+function restar() {
+  return num1 - num2;
+}
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
