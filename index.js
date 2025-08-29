62f1cee1c659d91003d90fa061a3887e1b60b5a9
Jean Carlo Salazar Gracia <larserxd@gmail.com>
2025-08-29 17:10:49 -0500

    division

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function multiplicar(num1, num2) {
   return num1 * num2;
 }
 
+function dividir(num1, num2) {
+  return num1/num2
+}
+
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
